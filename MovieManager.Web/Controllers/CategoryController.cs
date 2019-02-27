55268b0614a3099e202c116fd25b6925b2f5d3c9
knromaric <nzekeng@gmail.com>
2019-02-27 09:54:34 +0200

    Add a Bootstrap Theme + MVC controller

---
+++ MovieManager.Web/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieManager.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(Guid id)
+        {
+            return View(id);
+        }
+    }
+}
